4a79c222ee22f9b8f2617f62f6cf34cc881aaee0
Giuel <90395879+Giuel@users.noreply.github.com>
2021-11-07 15:55:20 +0100

    Starting UI mockup

--- JustIT/src/main/java/it/giulio/rustia/ispw/justit/HelloApplication.java
+++ JustIT/src/main/java/it/giulio/rustia/ispw/justit/HelloApplication.java
@@ -10,10 +10,11 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("justITWelcomePage.fxml"));
+        Scene welcomePage1 = new Scene(fxmlLoader.load());
+        stage.setTitle("Just-I.T.");
+        stage.setResizable(false);
+        stage.setScene(welcomePage1);
         stage.show();
     }
 
--- JustIT/src/main/java/it/giulio/rustia/ispw/justit/HelloController.java
+++ JustIT/src/main/java/it/giulio/rustia/ispw/justit/HelloController.java
@@ -1,14 +1,39 @@
 package it.giulio.rustia.ispw.justit;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
-
+   private Button bottoneAccesso;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+   private Hyperlink signUpLink;
+
+    public void showLoginPage(ActionEvent actionEvent) throws IOException {
+        Parent
+        Stage stage= new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("justITLoginPage.fxml"));
+        Scene loginPage= new Scene(fxmlLoader.load());
+        stage.setResizable(false);
+        stage.setTitle("Just It Login");
+        stage.setScene(loginPage);
+        stage.show();
+    }
+
+    public void showSignupPage(ActionEvent actionEvent) {
+    }
+
+    public void login(ActionEvent actionEvent) {
+    }
+
+    public void showWelcomePage(ActionEvent actionEvent) {
     }
 }
