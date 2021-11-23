4e288d0397492185988873f1fd65a2482e318543
Giuel <90395879+Giuel@users.noreply.github.com>
2021-11-23 13:38:14 +0100

    working on travis i hope

--- JustIT/src/main/java/it/giulio/rustia/ispw/justit/UIController2.java
+++ JustIT/src/main/java/it/giulio/rustia/ispw/justit/UIController2.java
@@ -1,16 +1,27 @@
 package it.giulio.rustia.ispw.justit;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
-public class UIController2 {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class UIController2 implements Initializable {
     @FXML
     private Button profileButton;
     private Stage stage;
     private Parent root;
+    private String link ="https://www.google.it/maps/";
     public void openProfile(){
         profileButton.setText("Mi Hai cliccato dal secondo controller");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        profileButton.setVisible(false);
+
     }
 }
